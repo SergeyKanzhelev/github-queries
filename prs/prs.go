fa8bf8ea657a8cccd2ab648b952ac18b50548474
Sergey Kanzhelev <S.Kanzhelev@live.com>
2021-06-23 22:49:15 +0000

    bugs stats

--- prs/prs.go
+++ prs/prs.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"io"
 	"time"
 
 	"golang.org/x/net/context"
@@ -22,6 +23,8 @@ type column struct {
 	Labels     string
 }
 
+var apiRequestsCount = 0
+
 func getPRsCount(query string) (int, error) {
 	q := url.Values{}
 	q.Add("q", query)
@@ -29,13 +32,21 @@ func getPRsCount(query string) (int, error) {
 
 	resp, err := http.Get("https://api.github.com/search/issues?" + q.Encode())
 
+	if apiRequestsCount == 9 {
+		time.Sleep(1 * time.Minute)
+		apiRequestsCount = 0
+	} else {
+		apiRequestsCount += 1
+	}
+
 	if err != nil {
 		return -1, fmt.Errorf("failed to get PRs: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		return -1, fmt.Errorf("status code is not 200: %v", resp.StatusCode)
+		b, _ := io.ReadAll(resp.Body)
+		return -1, fmt.Errorf("status code is not 200: %v, %v", resp.StatusCode, string(b))
 	}
 
 	var result = prs{}
@@ -93,7 +104,57 @@ func getPRs() ([]interface{}, error) {
 	return result, nil
 }
 
-func writeToSheet(values []interface{}) error {
+
+func getBugs() ([]interface{}, error) {
+	// see documentation
+	// https://developer.github.com/v3/search/#search-issues-and-pull-requests
+	// https://docs.github.com/en/github/searching-for-information-on-github/searching-issues-and-pull-requests
+
+	baseQuery := "repo:kubernetes/kubernetes is:issue is:open label:sig/node "
+
+	var dateNow = time.Now().UTC()
+
+	var dateNowStr = dateNow.Format("2006-01-02T15:04:05-0700")
+	var dateRange1day = dateNow.AddDate(0, 0, -1).UTC().Format("2006-01-02T15:04:05-0700") + ".." + dateNowStr
+	var dateRange10days = dateNow.AddDate(0, 0, -10).UTC().Format("2006-01-02T15:04:05-0700") + ".." + dateNowStr
+	var dateRange90days = dateNow.AddDate(0, 0, -90).UTC().Format("2006-01-02T15:04:05-0700") + ".." + dateNowStr
+	var dateOver90days = dateNow.AddDate(0, 0, -90).UTC().Format("2006-01-02T15:04:05-0700")
+
+	columns := []column{
+		column{"total", baseQuery},
+		column{"kind bug", baseQuery + "label:kind/bug"},
+		column{"kind cleanup", baseQuery + "label:kind/cleanup"},
+		column{"kind deprecation", baseQuery + "label:kind/deprecation"},
+		column{"kind documentation", baseQuery + "label:kind/documentation"},
+		column{"kind failing-test", baseQuery + "label:kind/failing-test"},
+		column{"kind feature", baseQuery + "label:kind/feature"},
+		column{"kind support", baseQuery + "label:kind/support"},
+		column{"kind flake", baseQuery + "label:kind/flake"},
+		column{"kind other", baseQuery + "-label:kind/bug -label:kind/cleanup -label:kind/deprecation -label:kind/design -label:kind/documentation -label:kind/failing-test -label:kind/feature -label:kind/support -label:kind/flake"},
+
+		column{"updated last day", baseQuery + "updated:" + dateRange1day},
+		column{"updated last 10 days", baseQuery + "updated:" + dateRange10days},
+		column{"updated last 90 days", baseQuery + "updated:" + dateRange90days},
+		column{"updated over 90 days", baseQuery + "updated:<" + dateOver90days},
+	}
+
+	header := "time"
+	result := []interface{}{}
+	result = append(result, fmt.Sprintf("%s", time.Now().Format("01/02/2006 15:04")))
+	for _, v := range columns {
+		count, err := getPRsCount(v.Labels)
+		if err != nil {
+			return nil, fmt.Errorf("error for query %s: %v", v.Labels, err)
+		}
+		result = append(result, count)
+		header += fmt.Sprintf(", \"%s\"", v.ColumnName)
+	}
+
+	return result, nil
+}
+
+
+func writeToSheet(values []interface{}, sheet string) error {
 	// Service account based oauth2 two legged integration
 	ctx := context.Background()
 	srv, err := sheets.NewService(ctx, option.WithCredentialsFile("credentials.json"), option.WithScopes(sheets.SpreadsheetsScope))
@@ -103,13 +164,13 @@ func writeToSheet(values []interface{}) error {
 
 	// https://docs.google.com/spreadsheets/d/1VW5_Eq8MzswfDi9xEvfYyP8edF_Ny7MBANIsJXT3VGw/edit
 	spreadsheetId := "1VW5_Eq8MzswfDi9xEvfYyP8edF_Ny7MBANIsJXT3VGw"
-	readRange := "Sheet1!A2:K"
+	readRange := sheet + "!A2:K"
 	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
 	if err != nil {
 		return fmt.Errorf("unable to retrieve data from sheet: %v", err)
 	}
 
-	writeRange := fmt.Sprintf("Sheet1!A%d", len(resp.Values)+2)
+	writeRange := fmt.Sprintf(sheet + "!A%d", len(resp.Values)+2)
 
 	var vr sheets.ValueRange
 
@@ -130,11 +191,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = writeToSheet(results)
+	err = writeToSheet(results, "Sheet1")
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 
 	fmt.Printf("%v\n", results)
+
+	bugs, err := getBugs()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = writeToSheet(bugs, "Bugs")
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%v\n", bugs)
 }
